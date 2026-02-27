e03c89711dfa2bbfe71f9b88da9cb89f09be4b86
madhu <madhuramyakondeti@gmail.com>
2026-02-27 12:01:26 +0530

    second commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app=express();
 app.set('views',path.join(__dirname,"views"));
 app.set('view engine','pug');
 app.get('/home',(req,res)=>{
-const subjects=['DevOps','CC','SPM','CNS'];
+const subjects=['DevOps','CC','SPM','CNS','CC LAB','CNSLAB','CD'];
 res.render("sample.pug",{'sub':subjects});
 })
 app.listen(4000,()=>{
